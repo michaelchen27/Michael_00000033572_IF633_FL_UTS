5f037c3ada36bb2a16b429c1dd3f993177d8808e
Michael <michael.chen2701@gmail.com>
2021-03-11 00:46:40 +0700

    simple homepage and simple profile

--- app/src/main/java/umn/ac/id/MainActivity.java
+++ app/src/main/java/umn/ac/id/MainActivity.java
@@ -2,13 +2,37 @@ package umn.ac.id;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button profile_button;
+    private Button login_button;
+
+    public MainActivity() { }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button profile_button = (Button) findViewById(R.id.profile_button);
+        Button login_button = (Button) findViewById(R.id.login_button);
+
+        profile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            }
+        });
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/umn/ac/id/ProfileActivity.java
@@ -0,0 +1,36 @@
+package umn.ac.id;
+
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
+public class ProfileActivity extends AppCompatActivity {
+    private TextView ref1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        TextView ref1 = (TextView) findViewById(R.id.coding_with_evan);
+        ref1.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
