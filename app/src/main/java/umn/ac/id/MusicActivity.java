e467ca9e09ffd92596cbb735661a863ae9b52855
Michael <michael.chen2701@gmail.com>
2021-03-11 22:31:56 +0700

    play and pause song working

--- app/src/main/java/umn/ac/id/MusicActivity.java
+++ app/src/main/java/umn/ac/id/MusicActivity.java
@@ -1,41 +1,154 @@
 package umn.ac.id;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class MusicActivity extends AppCompatActivity {
-//    View popUp;
-//    boolean click = true;
-    private LinearLayout music_page;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-    private AlertDialog.Builder dialogBuilder;
-    private AlertDialog dialog;
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicActivity extends AppCompatActivity {
+    ListView listView;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+
+        listView = findViewById(R.id.listViewSong);
+        runtimePermission();
+
+
     }
 
+    //Popup Window Dialog
     public void onStart() {
         super.onStart();
-        dialogBuilder = new AlertDialog.Builder(this);
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         final View popupView = getLayoutInflater().inflate(R.layout.popup_window, null);
 
         dialogBuilder.setView(popupView);
-        dialog = dialogBuilder.create();
+        AlertDialog dialog = dialogBuilder.create();
         dialog.show();
     }
 
+    //Permission
+    public void runtimePermission() {
+        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                displaySongs();
+
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+    public ArrayList<File> findSong (File file) {
+        ArrayList<File> arrayList = new ArrayList<>();
+
+        File[] files = file.listFiles();
+        for (File singlefile: files) {
+            if (singlefile.isDirectory() && !singlefile.isHidden())  {
+                arrayList.addAll(findSong(singlefile));
+            } else {
+                if(singlefile.getName().endsWith(".mp3") || singlefile.getName().endsWith(".wav")) {
+                    arrayList.add(singlefile);
+                }
+            }
+        }
+        return arrayList;
+    }
+
+    void displaySongs() {
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+
+        items = new String[mySongs.size()];
+        for (int i = 0; i < mySongs.size(); i++) {
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+        }
+//        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+//        listView.setAdapter(myAdapter);
+
+        customAdapter customAdapter = new customAdapter();
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String songName = (String) listView.getItemAtPosition(i);
+                startActivity(new Intent(getApplicationContext(), PlayerActivity.class)
+                        .putExtra("songs", mySongs)
+                        .putExtra("songname", songName)
+                        .putExtra("pos", i));
+            }
+        });
+    }
+
+    class customAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View myView = getLayoutInflater().inflate(R.layout.list_item, null);
+            TextView textsong = myView.findViewById(R.id.txtsongname);
+            textsong.setSelected(true);
+            textsong.setText(items[i]);
+
+            return myView;
+        }
+    }
 }
---
+++ app/src/main/java/umn/ac/id/PlayerActivity.java
@@ -0,0 +1,79 @@
+package umn.ac.id;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.gauravk.audiovisualizer.visualizer.BarVisualizer;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class PlayerActivity extends AppCompatActivity {
+    Button btnplay, btnnext, btnprev, btnff, btnfr;
+    TextView txtname, txtstart, txtstop;
+    SeekBar seekmusic;
+    BarVisualizer visualizer;
+
+    String sname;
+    public static final String EXTRA_NAME = "song_name";
+    static MediaPlayer mediaPlayer;
+    int position;
+    ArrayList<File> mySongs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        btnprev = findViewById(R.id.btnprev);
+        btnnext = findViewById(R.id.btnnext);
+        btnplay = findViewById(R.id.playbtn);
+        btnff = findViewById(R.id.btnff);
+        btnfr = findViewById(R.id.btnfr);
+        txtname = findViewById(R.id.txtsn);
+        txtstart = findViewById(R.id.txtstart);
+        txtstop = findViewById(R.id.txtstop);
+        seekmusic = findViewById(R.id.seekbar);
+        visualizer = findViewById(R.id.blast);
+
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+
+        Intent i = getIntent();
+        Bundle bundle = i.getExtras();
+
+        mySongs = (ArrayList) bundle.getParcelableArrayList("songs");
+        String songName = i.getStringExtra("songname");
+        position = bundle.getInt("pos", 0);
+        txtname.setSelected(true);
+        Uri uri = Uri.parse(mySongs.get(position).toString());
+        sname = mySongs.get(position).getName();
+        txtname.setText(sname);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+        mediaPlayer.start();
+
+        btnplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+                    btnplay.setBackgroundResource(R.drawable.ic_play);
+                    mediaPlayer.pause();
+                } else {
+                    btnplay.setBackgroundResource(R.drawable.ic_pause);
+                    mediaPlayer.start();
+                }
+            }
+        });
+    }
+}
